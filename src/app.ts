8d0e259c417ac320df3d308d4bc96d630b9661c0
Jean Paul Elisa NIYOKWIZERWA <mugishajeanbienvenu@gmail.com>
2024-04-20 20:31:51 +0200

    Home message

--- src/app.ts
+++ src/app.ts
@@ -48,13 +48,30 @@ Documentation();
 // seedSubscriber();
 
 app.get("/", (req, res) => {
-  res
-    .status(200)
-    .send(
-      "Welcome to MyBrand API.\n\nUse the following Endpoints to interact with the API:\n\n /api-docs, /api/blog/, /api/category/, /api/comment,\n /api/message, /api/notification, /api/subscriber/, /api/user"
-    );
+  res.status(200).send(`
+    <html>
+      <head>
+        <title>MyBrand API</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="<KEY>" crossorigin="anonymous">
+      </head>
+      <body>
+        <div class="container" style.margin-top=10px>
+          <div class="jumbotron">
+            <h1 class="display-4">Welcome to MyBrand API</h1>
+            <p class="lead">Use the following Endpoints to interact with the API:</p>
+            <hr class="my-4">
+            <ul>
+            <li>/api-docs</li> <li>/api/blog/</li> <li>/api/category/</li> <li>/api/comment</li> <li>/api/message</li> <li>/api/notification</li> <li>/api/subscriber/</li> <li>/api/user</li>
+            </ul>
+          </div>
+        </div>
+      </body>
+    </html>
+  `);
 });
-
 // Start the server
 const PORT: number = Number(process.env.PORT) || 6090;
 app.listen(PORT, () => {
