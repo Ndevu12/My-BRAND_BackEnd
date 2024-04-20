d2c7ad24bb4b252f98ce233f13d3a7e141836743
Jean Paul Elisa NIYOKWIZERWA <mugishajeanbienvenu@gmail.com>
2024-04-20 22:31:19 +0200

    fix: adding jest&node types

--- types/custom.d.ts
+++ types/custom.d.ts
@@ -8,3 +8,7 @@ declare module "swagger-ui-express";
 declare module "jsonwebtoken";
 declare module "crypto-js";
 declare module "multer";
+declare module "node";
+declare module "jest";
+declare module "mongoose";
+declare module "bcrypt";
